b408fa7ced4e5e0615be76c67c7c2aa9ee014e55
Ihor <ihorfront88@gmail.com>
2022-01-30 23:21:46 +0200

    added textContent to variable

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@ const catefories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${catefories.length}`);
 
 catefories.forEach((category) => {
-    const textHeader = category.firstElementChild;
+    const textHeader = category.firstElementChild.textContent;
     const amountElements = category.lastElementChild.children.length;
-    console.log(`Category:`, textHeader.textContent);
+    console.log(`Category:`, textHeader);
     console.log(`Elements:`, amountElements);
 });
