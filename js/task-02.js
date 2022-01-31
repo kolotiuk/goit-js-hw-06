5200d7c093e335bd977282ea1a6d8d46b6543b68
Ihor <ihorfront88@gmail.com>
2022-01-31 23:35:26 +0200

    added task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+ingredients.forEach((ingredient) => {
+    const listIngredients = document.querySelector("#ingredients");
+    const itemElement = document.createElement("li");
+    itemElement.innerHTML = ingredient;
+    listIngredients.append(itemElement);
+});
