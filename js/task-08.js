9e5e9081b2e1e3ed315fc91450fb8e1d83cdf17a
Ihor <ihorfront88@gmail.com>
2022-02-03 22:50:37 +0200

    added task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const login = document.querySelector(".login-form");
 
+const onSubmit = (event) => {
+    event.preventDefault();
+
+    const inputEmail = event.target.elements.email;
+    const inputPassword = event.target.elements.password;
+
+    if (inputEmail.value.trim() || inputPassword.value.trim()) {
+        const formData = {
+            email: inputEmail.value,
+            password: inputPassword.value,
+        };
+        console.log(formData);
+        login.reset();
+        return;
+    }
+
+    alert("Все поля должны быть заполнены.");
+};
+
+login.addEventListener("submit", onSubmit);
