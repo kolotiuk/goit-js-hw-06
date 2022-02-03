1cf7a3d4edf1a6c914c04312ce2464e2353e4ae6
Ihor <ihorfront88@gmail.com>
2022-02-03 23:03:17 +0200

    added task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+    buttonChangeColor: document.querySelector(".change-color"),
+    colorValue: document.querySelector(".color"),
+    body: document.body,
+};
+
+const onClickColorChange = () => {
+    const bodyElementColor = (refs.body.style.backgroundColor =
+        getRandomHexColor());
+    refs.colorValue.textContent = bodyElementColor;
+};
+
+refs.buttonChangeColor.addEventListener("click", onClickColorChange);
