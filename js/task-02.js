38266a832795221c42dd7d9dd45e5eced7e18c76
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-04-11 20:54:59 +0300

    update task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,19 +1,12 @@
-const ingredients = [
-    "Potatoes",
-    "Mushrooms",
-    "Garlic",
-    "Tomatos",
-    "Herbs",
-    "Condiments",
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
-const listEl = document.querySelector("#ingredients");
+const listRef = document.querySelector('#ingredients');
 
-const liItems = ingredients.map((ingredient) => {
-    const itemEl = document.createElement("li");
-    itemEl.classList.add = "item";
-    itemEl.textContent = ingredient;
-    return itemEl;
+const markup = ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item;
 });
 
-listEl.append(...liItems);
+listRef.append(...markup);
