f1aff8f7b410ec901b88d5a3346f75c573ebf30c
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-04-12 23:37:08 +0300

    update task-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,11 @@
-const refs = {
-    input: document.querySelector("#name-input"),
-    output: document.querySelector("#name-output"),
-};
+const inputRef = document.querySelector('#name-input');
+const spanRef = document.querySelector('#name-output');
 
-refs.input.addEventListener("input", (event) => {
-    refs.output.textContent = event.currentTarget.value
-        ? event.currentTarget.value
-        : "Anonymous";
+inputRef.addEventListener('input', () => {
+    if (inputRef.value.trim()) {
+        spanRef.textContent = inputRef.value;
+        return;
+    }
+
+    spanRef.textContent = 'Anonymous';
 });
