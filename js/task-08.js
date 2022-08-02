887d706133815631c040b820e6d40db5dbac7b8c
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-08-02 21:31:10 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,25 +1,22 @@
-const formRef = document.querySelector('.login-form');
-const inputEmail = document.querySelector('[type="email"]');
-const inputPass = document.querySelector('[type="password"]');
+const formRef = document.querySelector(".login-form");
 
-const onForm = e => {
+const onForm = (e) => {
     e.preventDefault();
 
-    if (!inputEmail.value.trim() || !inputPass.value.trim()) {
-        alert('все поля должны быть заполнены');
+    const { email, password } = e.target.elements;
+
+    if (!email.value.trim() || !password.value.trim()) {
+        alert("все поля должны быть заполнены");
         return;
     }
 
-    const formEmail = e.target.elements.email.value;
-    const formPassword = e.target.elements.password.value;
-
-    const formData = {
-        email: formEmail,
-        password: formPassword,
+    const obj = {
+        email: email.value,
+        password: password.value,
     };
-    console.log(formData);
+    console.log("~ obj", obj);
 
     e.target.reset();
 };
 
-formRef.addEventListener('submit', onForm);
+formRef.addEventListener("submit", onForm);
