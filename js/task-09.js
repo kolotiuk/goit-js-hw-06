ec0bf5dd64a4a48355232696494d5ed50747857b
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-08-03 19:50:13 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,13 @@
+const btnChangeRef = document.querySelector(".change-color");
+const textColorRef = document.querySelector(".color");
+
+const onChange = () => {
+    textColorRef.textContent = getRandomHexColor();
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
+btnChangeRef.addEventListener("click", onChange);
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const refs = {
-    buttonChangeColor: document.querySelector(".change-color"),
-    colorValue: document.querySelector(".color"),
-    body: document.body,
-};
-
-const onClickColorChange = () => {
-    const bodyElementColor = (refs.body.style.backgroundColor =
-        getRandomHexColor());
-    refs.colorValue.textContent = bodyElementColor;
-};
-
-refs.buttonChangeColor.addEventListener("click", onClickColorChange);
